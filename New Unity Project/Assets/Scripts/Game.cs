52f82f24f5a233bae14e89b1891305ea6b1682a8
Oneliner12 <bartek1237@gmail.com>
2018-10-11 12:11:11 +0200

    Scheduler

---
+++ New Unity Project/Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+       Scheduler._Instance.Scheduleparameter(4, "delay message");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Scheduler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scheduler : MonoBehaviour {
+
+    private void Awake()
+    {
+        _Instance = this;
+    }
+    public void Start()
+    {
+
+    }
+    public static Scheduler _Instance { get; private set; }
+
+    public void Scheduleparameter(float delay, string message)
+    {
+        Debug.Log("delay is " + delay);
+    }
+
+    private IEnumerator RunDelay(float delay, string message)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("DelayedAttribute message:" + message);
+
+    }
+}
